49816797c92ef384741b7bf4815614e8aa9616d6
Spencer Lyon <spencerlyon2@gmail.com>
2020-07-31 15:03:14 -0700

    ENH: bump to version 0.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="covidcountydata",
-    version="0.2.0",
+    version="0.4.0",
     description="Python client for accessing the COVID County Data database",
     long_description=long_description,
     long_description_content_type="text/markdown",
